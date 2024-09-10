1c8a515ff45b8d94b05e61cc2d19e1f7418478fe
ErikGBravo <bravo.guerrero.erik@gmail.com>
2024-09-09 18:12:45 -0600

    se agrega archivo de configuracion base

--- dist/app.js
+++ dist/app.js
@@ -3,25 +3,25 @@ class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
-        //Este metodo se encarga de  modificar o renderizar desde typescript  el node del doom
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.element.id = 'user-input';
         this.titleInputElement = this.element.querySelector('#title');
         this.descriptionInputElement = this.element.querySelector('#description');
         this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
-    //generamos el evento  manejador de forma privada
     submitHandler(event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
     }
     configure() {
-        this.element.addEventListener('submit', this.submitHandler);
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 }
 const prjInput = new ProjectInput();
+//# sourceMappingURL=app.js.map
