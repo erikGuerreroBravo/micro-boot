8aeaca3b6a57389d2d837f7540aa46b61c86996e
ErikGBravo <bravo.guerrero.erik@gmail.com>
2024-09-11 00:10:43 -0600

    se modifican los elementos base de la validadion para uitilizar interfaces y transportar losd atos.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
+//Validacion
+interface Validatable{
+
+    value: string| number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?:number;
+}
+function validate( validatableInput: Validatable){
+    let isValid = true;
+    if( validatableInput.required){
+        isValid = isValid && validatableInput.value.toString().trim().length !==0;
+    }
+    if(validatableInput.minLength != null && typeof validatableInput.value === 'string'){
+        isValid = validatableInput.value.length >= validatableInput.minLength;
+    }
+    if(validatableInput.maxLength !=null && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if(validatableInput.min !=null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }  
+    if(validatableInput.max !=null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }    
+
+    return isValid;
+}
 //agregamos el decorador
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
     
@@ -34,11 +64,12 @@ class ProjectInput{
         this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
         this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        //este metodo es el evento de escucha
         this.configure();
+        //adjuntamos todos los elementos dentro del hijo del nodo de html
         this.attach();
     }
-
-
+    ///metodo que se encarga  de limpiar los campos del form
     private clearFields(){
         this.titleInputElement.value = "";
         this.descriptionInputElement.value = "";
@@ -49,7 +80,23 @@ class ProjectInput{
         const enteredTitle =this.titleInputElement.value;
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople =  this.peopleInputElement.value;
-        if( enteredTitle.trim().length ===0 ||   enteredDescription.trim().length === 0 ||  enteredPeople.trim().length ===0){
+
+        const titleValidatable : Validatable ={
+            value:enteredTitle,
+            required: true
+        };
+        const descriptionValidatable: Validatable ={
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+       const peopleValidatable: Validatable = {
+           value: +enteredPeople,
+           required: true,
+           min: 1,
+           max:5
+       };
+       if( !validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable) ){
             alert("Ingresa los valores correctos, no se permiten nulos");
             return;
         }
@@ -59,6 +106,7 @@ class ProjectInput{
     }
 
     private attach(){
+        //se encarga de insertar un nodo de acuerdo a la posicion especifica en la cual se agrega dicho nodo.
         this.hostElement.insertAdjacentElement('afterbegin',this.element);
     }
    //generamos el evento  manejador de forma privada
