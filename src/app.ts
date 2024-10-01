b026de62479877f53290499257d7a76b063906dd
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2024-10-01 02:47:13 -0600

    se modifican todos los elementos de interfaz de usuario, utilizando elementos del tipo drag and drop

--- src/app.ts
+++ src/app.ts
@@ -171,7 +171,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement>
 }
 
 //creamos el componente de la lista
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable{
    
    private project: Project;
    
@@ -192,9 +192,23 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
        this.project = project;
        this.configure();
        this.renderContent();
-}
+   }
+   ///necesitamos  agregar dos elementos  mas para el drag and drop
+   //nos ayuda a construir elementos mas arrastrables  de manera uniforme
+   @autobind  ///agregamos el elemento del tipo binding
+   dragStartHandler(event: DragEvent): void {
+       console.log(event);
+   }
+   dragEndHandler(_: DragEvent): void {
+       console.log('DragEnd');
+   }
+
+
 configure(): void {
-    
+    ///agregamos el elemento necesario para escuchar  cuando hay un elemento de arrastrar y soltar.
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    //agregamos el elemento que termina  el proceso  de soltar 
+    this.element.addEventListener('dragend', this.dragEndHandler);
 }
 renderContent(): void {
     this.element.querySelector('h2')!.textContent = this.project.title;
